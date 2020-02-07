836952e3404252d3033276bc9e99a7fa73b0d724
Ilia Maystrenko <ii.maistrenko@severstal-ssc.ru>
2020-02-07 18:27:09 +0300

    qwe

---
+++ script.py
@@ -0,0 +1,23 @@
+import socket
+import os
+
+# Задаем адрес сервера
+SERVER_ADDRESS = ('0.0.0.0', os.environ.get(['PORT'], 8443))
+
+# Настраиваем сокет
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind(SERVER_ADDRESS)
+server_socket.listen(10)
+print('server is running, please, press ctrl+c to stop')
+
+# Слушаем запросы
+while True:
+    connection, address = server_socket.accept()
+    print("new connection from {address}".format(address=address))
+
+    data = connection.recv(1024)
+    print(str(data))
+
+    connection.send(bytes('Hello from server!', encoding='UTF-8'))
+
+    connection.close()
